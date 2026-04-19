40c2bb7e41188b9f2190a47dc28cad2b79597cbd
Daniel-Adolfo-Abarca-Ureña <daniel.abarca.urena@est.una.ac.cr>
2026-04-18 20:15:53 -0600

    Refactor build.gradle.kts for group ID and repositories
    Updated group ID to lowercase and modified repository configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,34 +3,32 @@ plugins {
   id("maven-publish")
 }
 
-group = "io.github.DanielUNA"
+group = "io.github.danieluna"
 version = "0.12.0"
 
-repositories {
-  mavenCentral()
-}
-
 publishing {
-  publications {
-    create<MavenPublication>("mavenJava") {
-      from(components["java"])                // attach the Java artifact (jar)
-      pom {
-        name.set("DAdapterForNativeTTS")
-        url.set("https://github.com/Daniel-UNA/DAdapterForNativeTTS")
-        scm {
-          url.set("https://github.com/Daniel-UNA/DAdapterForNativeTTS")
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "jadapterfornativetts" // todo en minúsculas
+            pom {
+                name.set("DAdapterForNativeTTS")
+                url.set("https://github.com/Daniel-UNA/DAdapterForNativeTTS")
+                scm {
+                    url.set("https://github.com/Daniel-UNA/DAdapterForNativeTTS")
+                }
+            }
         }
-      }
     }
-  }
-repositories {
-    maven {
-      name = "DAdapterForNativeTTS"
-      url = uri("https://github.com/Daniel-UNA/DAdapterForNativeTTS")
-      credentials {
-        username = System.getenv("GITHUB_ACTOR")
-        password = System.getenv("GITHUB_TOKEN")
-      }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Daniel-UNA/DAdapterForNativeTTS")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
-  }
 }
